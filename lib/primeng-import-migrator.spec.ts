c4bb0fd78db14e037ca84ae539695a9c72d2586e
Konstantin Novikov <nov.konstantin@gmail.com>
2020-07-14 16:09:39 +0200

    matches unit tests

--- lib/primeng-import-migrator.spec.ts
+++ lib/primeng-import-migrator.spec.ts
@@ -1,40 +1,51 @@
 import { PrimengImportMigrator } from './primeng-import-migrator';
-import { FileSystem } from './file-system';
-import { resolve } from 'path';
 
 describe('PrimengImportMigrator', () => {
 
   let sut: PrimengImportMigrator;
-  let fileSystem: FileSystem;
 
   beforeEach(() => {
     sut = new PrimengImportMigrator();
-    fileSystem = new FileSystem();
   });
 
   it('should create', () => {
     expect(sut).toBeTruthy();
   });
 
-  it('should find all seperate primeng import statements', async () => {
-    const inputFilePath = resolve(__dirname, '../test-files/seperated-imports/actual.ts');
-    const inputFileContent = await fileSystem.read(inputFilePath);
-    const primengImports = await sut.allPrimengImports(inputFileContent);
-    expect(primengImports.length).toEqual(15);
-  });
-
-  it('should find all clustered primeng import statements', async () => {
-    const inputFilePath = resolve(__dirname, '../test-files/clustered-imports/actual.ts');
-    const inputFileContent = await fileSystem.read(inputFilePath);
-    const primengImports = await sut.allPrimengImports(inputFileContent);
-    expect(primengImports.length).toEqual(1);
+  it('should return an empty array if no matches are found', async () => {
+    const fileContent = `import {
+      RouterModule,
+      Routes
+    } from '@angular/router';`;
+    const actual = await sut.allPrimengImports(fileContent);
+    const expected = [];
+    expect(actual).toEqual(expected);
   });
 
   it('should find all mixed primeng imports', async () => {
-    const inputFilePath = resolve(__dirname, '../test-files/mixed-imports/actual.ts');
-    const inputFileContent = await fileSystem.read(inputFilePath);
-    const primengImports = await sut.allPrimengImports(inputFileContent);
-    expect(primengImports.length).toEqual(3);
+    const fileContent = `import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import { SharedModule } from '../shared/components/shared.module';
+import {
+  TreeModule,
+  ToggleButtonModule
+} from 'primeng/primeng';
+import { ButtonModule } from 'primeng/primeng';
+import { TableModule } from 'primeng/primeng';
+import { AdministrationGuard } from './guards/administration.guard';
+import { AdministrationService } from './services/administration.service';`;
+    const actual = await sut.allPrimengImports(fileContent);
+    const expected = [
+      `import {
+  TreeModule,
+  ToggleButtonModule
+} from 'primeng/primeng';`,
+      'import { ButtonModule } from \'primeng/primeng\';',
+      'import { TableModule } from \'primeng/primeng\';'
+    ];
+    expect(actual).toEqual(expected);
   });
 
 });
