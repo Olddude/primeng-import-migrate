903512f1b522a7000cd04ae344221c1613308e99
Konstantin Novikov <nov.konstantin@gmail.com>
2020-07-14 12:05:11 +0200

    extending filesystem functionality

--- lib/file-system.spec.ts
+++ lib/file-system.spec.ts
@@ -20,4 +20,31 @@ describe('FileSystem', () => {
     expect(actual).toContain(expected);
   });
 
+  it('should write data to file and delete it afterwards', async () => {
+    const path = resolve(__dirname, './deleteme.txt');
+    const data = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident,
+sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+    await sut.write(path, data);
+    expect(await sut.doesExist(path)).toBe(true);
+    await sut.delete(path);
+    expect(await sut.doesExist(path)).toBe(false);
+  });
+
+  it('should find all files with matching pattern', async () => {
+    const root = resolve(__dirname, '../test-files');
+    const files = await sut.findFiles(/.ts/, root);
+    expect(files.length).toEqual(4);
+  });
+
+  it('should not find any files if pattern does not match', async () => {
+    const root = resolve(__dirname, '../test-files');
+    const files = await sut.findFiles(/.exe/, root);
+    expect(files).toEqual([]);
+  });
+
 });
--- lib/file-system.ts
+++ lib/file-system.ts
@@ -1,8 +1,29 @@
-import { readFile } from 'fs';
+import { readFile, writeFile, unlink, exists } from 'fs';
 import { promisify } from 'util';
+import { file } from 'find';
 
 export class FileSystem {
   read(path: string): Promise<string> {
     return promisify(readFile)(path, { encoding: 'utf-8' });
+  }
+
+  write(path: string, data: string): Promise<void> {
+    return promisify(writeFile)(path, data, { encoding: 'utf-8' });
+  }
+
+  delete(path: string): Promise<void> {
+    return promisify(unlink)(path);
+  }
+
+  doesExist(path: string): Promise<boolean> {
+    return promisify(exists)(path);
+  }
+
+  findFiles(pattern: string | RegExp, root: string): Promise<string[]> {
+    return new Promise(resolve => {
+      file(pattern, root, (files) => {
+        resolve(files);
+      });
+    });
   }
 }
